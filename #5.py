d3cacd8ce94d58348a806c42e88571aaff29a177
Nafiul Islam <gamesbrainiac@gmai.com>
2013-06-02 22:38:07 +0600

    Attempt @ 5

--- #5.py
+++ #5.py
@@ -3,12 +3,19 @@ __title__ = 'Smallest Multiple'
 
 
 def main():
-    max = 1
-    true_flags = 0
-    for number in xrange(1, 10):
-        max *= number
-    print max
+    max_number = 1
+    list_of_possibilities = []
+    for number in xrange(1, 11):
+        max_number *= number
 
+    print max_number
+
+    while max_number % 2 == 0 and max_number != 0:
+        max_number /= 2
+        list_of_possibilities.append(max_number)
+        print max_number
+
+    print list_of_possibilities
 
 if __name__ == "__main__":
     main()
