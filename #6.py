5ab6e36f5bca029960ac93a180eca74d0be14c99
Nafiul Islam <gamesbrainiac@gmai.com>
2013-06-02 23:28:29 +0600

    Finished #6

--- #6.py
+++ #6.py
@@ -1,10 +1,26 @@
 __author__ = 'Nafiul Islam'
 __title__ = 'Sum square difference'
 
+import math
+
 
 def main(argv):
-    pass
+    sum_of_squares = 0
+    sum_squared = 0
 
+    # Getting the sum of individual squares
+    for i in xrange(1, argv+1):
+        sum_of_squares += i**2
+
+    # Getting total sum of numbers squared
+    for i in xrange(1, argv+1):
+        sum_squared += i
+
+    sum_squared **= 2
+
+    print "Sum of the squares:", sum_of_squares
+    print "Sum squared:", sum_squared
+    print long(math.fabs(sum_of_squares - sum_squared))
 
 if __name__ == "__main__":
-    main()
+    main(100)
