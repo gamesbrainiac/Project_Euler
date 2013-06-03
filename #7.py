b1f5140846366e0bdbc092b1b15c2e99ef94a06d
Nafiul Islam <gamesbrainiac@gmai.com>
2013-06-03 23:01:39 +0600

    Started #7

--- #7.py
+++ #7.py
@@ -2,3 +2,22 @@ __author__ = 'Nafiul Islam'
 __title__ = '10001st prime'
 
 
+def main(argv):
+    list_primes = [2]
+    number = 3
+    loop = 0  # performance check
+
+    while len(list_primes) < argv:
+        for val in list_primes:
+            if number % val == 0:
+                break
+            elif number % val != 0 and val == list_primes[-1]:
+                list_primes.append(number)
+        number += 1
+        loop += 1
+
+    print 'Prime @', argv, 'is', list_primes[-1]
+
+
+if __name__ == "__main__":
+    main(10001)
